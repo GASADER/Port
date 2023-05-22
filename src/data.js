ac3a198c02c5233cb0b7e03ca3c41fca2e020fe1
GASADER <kajorhkaen@gmail.com>
2023-05-22 20:34:55 +0700

    add:data

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ import { Icon } from "@iconify/react";
 
 // Projects Images (add your images to the images directory and import below)
 import Logo from "./images/logo.svg";
+import html from "./images/html-5 (1).png"
 
 // Hero Images (add your images to the /images directory with the same names)
 import HeroLight from "./images/hero-light.jpg";
@@ -15,7 +16,7 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "Your GitHub username here";
+export const githubUsername = "GASADER";
 
 // Blog link icon - https://icon-sets.iconify.design/
 export const Blog = <Icon icon="ph:link-bold" />;
@@ -25,7 +26,10 @@ export const Blog = <Icon icon="ph:link-bold" />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
+  `I'm a former hotel receptionist who has been working for 3 years, trained from the
+  Junior Software Developer Program Cohort 4 Generation Thailand program, which has skills in
+  HTML, CSS, JavaScript, MAEN Stack and Behavioral skills and mindsets of Software Developer,
+  Currently studying JAVA, Docker and looking for a job related to helping SMEs.`;
 
 /* Skills
  ************************************************************** 
@@ -34,23 +38,23 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
+    skill: <Icon icon="vscode-icons:file-type-html" className="display-4" />,
     name: "HTML5",
   },
   {
     id: 2,
-    skill: <Icon icon="ion:logo-css3" className="display-4" />,
+    skill: <Icon icon="vscode-icons:file-type-css" className="display-4" />,
     name: "CSS3",
   },
   {
     id: 3,
-    skill: <Icon icon="fa6-brands:js" className="display-4" />,
+    skill: <Icon icon="vscode-icons:file-type-js-official" className="display-4" />,
     name: "JavaScript",
   },
   {
     id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
-    name: "BootStrap",
+    skill: <Icon icon="vscode-icons:file-type-tailwind" className="display-4" />,
+    name: "Tailwind",
   },
   {
     id: 5,
@@ -59,39 +63,62 @@ export const skillData = [
   },
   {
     id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
+    skill: <Icon icon="vscode-icons:file-type-light-next" className="display-4" />,
+    name: "Next",
   },
   {
     id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
+    skill: <Icon icon="vscode-icons:file-type-node" className="display-4" />,
+    name: "Node.JS",
   },
   {
     id: 8,
-    skill: <Icon icon="bi:git" className="display-4" />,
-    name: "Git",
+    skill: <Icon icon="devicon:express-wordmark" className="display-4" />,
+    name: "Express",
   },
   {
     id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
+    skill: <Icon icon="vscode-icons:file-type-mongo" className="display-4" />,
+    name: "MongoDB",
+  },
+  {
+    id: 10,
+    skill: <Icon icon="vscode-icons:file-type-git" className="display-4" />,
+    name: "Git",
+  },
+  {
+    id: 11,
+    skill: <Icon icon="devicon:github" className="display-4" />,
     name: "GitHub",
+  },
+  {
+    id: 12,
+    skill: <Icon icon="logos:figma" className="display-4" />,
+    name: "Figma",
   },
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
-export const resume = null;
+export const resume = 'https://www.canva.com/design/DAFcmVeYqms/_tXZHWUP-BaCxIKGMphw0g/view?utm_content=DAFcmVeYqms&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink';
 
 /* Projects
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const filteredProjects = ["Colmer_responsiveWeb", "React-Project-Sprint02-I-1", "Promptpay-QR"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 4-5)
 export const projectCardImages = [
   {
-    name: "example-1",
+    name: "Colmer_responsiveWeb",
+    image: html,
+  },
+  {
+    name: "React-Project-Sprint02-I-1",
+    image: Logo,
+  },
+  {
+    name: "Promptpay-QR",
     image: Logo,
   },
 ];
@@ -101,4 +128,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/xknaagbo";
